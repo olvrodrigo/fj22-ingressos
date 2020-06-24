9651b4ba3d049664d7956cc90ddde1e57e5b29ac
rodrigo <rodrigo@pop-os.localdomain>
2020-06-24 18:43:31 -0300

    5.2 EXERCÍCIO - CRIANDO LÓGICA PARA TELA DE SELEÇÃO DE LUGARES

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,6 +3,8 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -23,18 +25,21 @@ public class Sessao {
 	private Filme filme;
 	private BigDecimal preco;
 
-	
 	/**
-	* @deprecated hibernate only
-	*/
+	 * @deprecated hibernate only
+	 */
 	public Sessao() {
 	}
-	
+
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
 		this.preco = sala.getPreco().add(filme.getPreco());
+	}
+
+	public Map<String, List<Lugar>> getMapaDeLugares() {
+		return sala.getMapaDeLugares();
 	}
 
 	public BigDecimal getPreco() {
